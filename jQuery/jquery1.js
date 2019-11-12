6b661ac9fd20a114150964641506d5f71e375227
BIT Student <classroom@bgit.rs>
2019-11-12 17:55:09 +0100

    jQuery

---
+++ jQuery/jquery1.js
@@ -0,0 +1,13 @@
+/* $("li:first").css({ "border-bottom": "1px solid" });
+
+$("ul").css({ "text-transform": "uppercase" });
+
+$(".active").css({ color: "red" });
+
+$("li")
+  .eq(2)
+  .addClass("bcgColor");
+
+$(function() {
+  console.log("Welcome");
+}); */
